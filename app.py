49c27cbd0eabf31302867a959328d8b4dfcd391c
Vikas Sharma <93978146+vikasharma005@users.noreply.github.com>
2023-08-31 12:51:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+
+import streamlit as st
+import folium
+from streamlit_folium import folium_static
+import urllib.request
+import json
+import geocoder
+
+def main():
+
+    with st.sidebar:
+      st.image("/content/iss.gif")
+      st.title("ISS Location Tracker")
+
+    # Fetch and display astronaut data
+    astronaut_data = fetch_astronaut_data()
+    st.write(f"There are currently {astronaut_data['number']} astronauts on the ISS:")
+    for person in astronaut_data['people']:
+        st.write(f"- {person['name']}")
+
+    # Display user's current lat/long
+    user_location = get_user_location()
+    st.write(f"Your current lat/long is: {user_location}")
+
+    # Display ISS location on a map
+    iss_lat, iss_lon = fetch_iss_location()
+    m = create_map(iss_lat, iss_lon)
+    folium_static(m)
+
+def fetch_astronaut_data():
+    url = "http://api.open-notify.org/astros.json"
+    response = urllib.request.urlopen(url)
+    result = json.loads(response.read())
+    return result
+
+def get_user_location():
+    g = geocoder.ip('me')
+    return g.latlng
+
+def fetch_iss_location():
+    url = "http://api.open-notify.org/iss-now.json"
+    response = urllib.request.urlopen(url)
+    result = json.loads(response.read())
+    location = result["iss_position"]
+    lat = float(location['latitude'])
+    lon = float(location['longitude'])
+    return lat, lon
+
+def create_map(lat, lon):
+    m = folium.Map(location=[lat, lon], zoom_start=2)
+    
+    # Add the ISS icon as a marker
+    icon = folium.CustomIcon(icon_image="/content/iss.png", icon_size=(50, 50))
+    folium.Marker([lat, lon], icon=icon, popup="ISS").add_to(m)
+
+    return m
+
+if __name__ == "__main__":
+    main()
+
